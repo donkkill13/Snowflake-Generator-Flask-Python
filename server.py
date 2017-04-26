3c9d6700af7f0f3da5a624cb4b80b4db92d9b478
NotSoSuper <xnotsosuper@gmail.com>
2017-04-26 15:57:07 -0400

    sublime tab detection good

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from snowflake import SnowflakeGenerator
-import json
+
 app = Flask(__name__)
 
 generator = SnowflakeGenerator(1, 1)
@@ -8,8 +8,8 @@ generator = SnowflakeGenerator(1, 1)
 @app.route("/", methods=['GET'])
 def index():
   load = {'snowflake': generator.generateID(), 'epoch_offset': generator.epoch_offset}
-	return jsonify(load)
+  return jsonify(load)
 
 if __name__ == "__main__":
-	context = ('certificate.crt', 'certificate.key')
-	app.run(host='0.0.0.0', port=443, ssl_context=context)
+  context = ('certificate.crt', 'certificate.key')
+  app.run(host='0.0.0.0', port=443, ssl_context=context)
--- snowflake.py
+++ snowflake.py
@@ -2,32 +2,32 @@ import time
 from math import floor
 
 '''
-	Offset from Unix Epoch
-	Unix Epoch : January 1 1970 00:00:00 GMT
-	Epoch Offset : September 9 2001 12:13:00 GMT
+  Offset from Unix Epoch
+  Unix Epoch : January 1 1970 00:00:00 GMT
+  Epoch Offset : September 9 2001 12:13:00 GMT
 '''
 
 class SnowflakeGenerator:
-	def __init__(self, datacenter_id, machine_id):
-		self.epoch_offset = 1000210380000
-		self.datacenter_id = datacenter_id
-		self.machine_id = machine_id
-		self.last_timestamp = self.getUnixTimestamp()
-		self.sequence_number = 1
+  def __init__(self, datacenter_id, machine_id):
+    self.epoch_offset = 1000210380000
+    self.datacenter_id = datacenter_id
+    self.machine_id = machine_id
+    self.last_timestamp = self.getUnixTimestamp()
+    self.sequence_number = 1
 
-	def generateID(self):
-		timestamp = int(self.getUnixTimestamp() - self.epoch_offset)
-		sequence = self.getNextSequence(timestamp)
-		self.last_timestamp = timestamp
-		snowflake_id = (timestamp << 22) | (self.datacenter_id << 5) | (self.machine_id << 5) | (sequence << 12)
-		return int(snowflake_id)
+  def generateID(self):
+    timestamp = int(self.getUnixTimestamp() - self.epoch_offset)
+    sequence = self.getNextSequence(timestamp)
+    self.last_timestamp = timestamp
+    snowflake_id = (timestamp << 22) | (self.datacenter_id << 5) | (self.machine_id << 5) | (sequence << 12)
+    return int(snowflake_id)
 
-	def getUnixTimestamp(self):
-		return floor(time.time() * 1000)
+  def getUnixTimestamp(self):
+    return floor(time.time() * 1000)
 
-	def getNextSequence(self, timestamp):
-		if self.last_timestamp == timestamp:
-			self.sequence_number += 1
-		else:
-			self.sequence_number = 1
-		return self.sequence_number
+  def getNextSequence(self, timestamp):
+    if self.last_timestamp == timestamp:
+      self.sequence_number += 1
+    else:
+      self.sequence_number = 1
+    return self.sequence_number
